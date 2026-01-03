1a6ebdb09277c2c92be6132490196c024dbc18c6
Bernardo <bernardo.fichera@gmail.com>
2026-01-03 14:30:22 +0100

    set pixelsize

--- config.h
+++ config.h
@@ -8,8 +8,8 @@ static const unsigned int snap = 32; /* snap pixel */
 static const int swallowfloating = 0; /* 1 means swallow floating windows by default */
 static const int showbar = 1; /* 0 means no bar */
 static const int topbar = 1; /* 0 means bottom bar */
-static const char* fonts[] = {"JetBrainsMono Nerd Font:size=12"};
-static const char dmenufont[] = "JetBrainsMono Nerd Font:size=12";
+static const char* fonts[] = {"JetBrainsMono Nerd Font:pixelsize=24"};
+static const char dmenufont[] = "JetBrainsMono Nerd Font:pixelsize=24";
 
 static const char col_gray1[] = "#222222";
 static const char col_gray2[] = "#444444";
